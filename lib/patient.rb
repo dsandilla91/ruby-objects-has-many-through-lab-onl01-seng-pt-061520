c4f585e1dd99c7e846cf7ac6c31a9f310aa3156a
Deborah Santillan <s.debb@yahoo.com>
2020-07-26 16:31:20 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,16 @@
+class Patient
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name=name
+  end
+  
+   def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
